e005cb41893aafaef902fb01cbc780bd79c8b1d5
Bryan Austyn Ichsan <101229576@students.swinburne.edu.my>
2022-04-08 11:57:02 +0700

    Update database details in settings.py

--- ciso_back_end/settings.py
+++ ciso_back_end/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ciso_back_end.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cloud',
+        'USER':'postgres',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
