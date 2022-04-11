5de268937aa8c6dcb98655aa42112a3309c184e1
xlwschn <tidus144@gmail.com>
2022-04-11 09:24:22 +0800

    update

--- database/models.py
+++ database/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Instance(models.Model):
-    id = models.CharField(max_length=100, unique=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
     status = models.CharField(max_length=100, unique=True)
     ipv4 = models.CharField(max_length=100, unique=True)
