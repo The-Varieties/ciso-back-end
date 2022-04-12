718228589c00753bd5b7242f706defb7a681049f
xlwschn <tidus144@gmail.com>
2022-04-12 13:26:34 +0800

    update

--- database/urls.py
+++ database/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 urlpatterns=[
 
-    path(r'<instance>',views.instanceApi),
-    path(r'<instance/([0-9]+)>',views.instanceApi),
+    path(r'<instance>/',views.instanceApi),
+    path(r'<instance/([0-9]+)>/',views.instanceApi),
 
-    path(r'<instance/savefile>',views.SaveFile)
+    path(r'<instance/savefile>/',views.SaveFile)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
