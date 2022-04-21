d9253f5efa644a3d181054159aa1a15acdf215cd
Bryan Austyn Ichsan <101229576@students.swinburne.edu.my>
2022-04-21 21:50:16 +0700

    Add tests for the endpoints

--- rightsizing_recommendation/tests.py
+++ rightsizing_recommendation/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from .models import *
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+
+client = Client()
 
 class RightSizingModels(TestCase):
     def test_node_cpu_tables_count(self):
@@ -17,5 +22,23 @@ class RightSizingModels(TestCase):
     def test_node_uname_count(self):
         self.assertNotEqual(0, NodeUnameInfo.objects.count())
         
-# class RightSizingUtils(TestCase):
-    
+class RightSizingUtils(TestCase):
+    def test_get_cpu_usage(self):
+        response = client.get(reverse('get-cpu-usage'), {"instance": "node_exporter", "time_interval": "5 minutes"})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_ram_usage(self):
+        response = client.get(reverse('get-ram-usage'), {"instance": "node_exporter", "time_interval": "5 minutes"})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_server_info(self):
+        response = client.get(reverse('get-server-info'), {"instance": "node_exporter"})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_usage_category(self):
+        response = client.get(reverse('get-usage-category'), {"instance": "node_exporter", "time_interval": "5 minutes"})
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
