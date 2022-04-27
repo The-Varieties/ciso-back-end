e63c15551617afff53917e63080f8adbea6d7f5f
Bryan Austyn Ichsan <101229576@students.swinburne.edu.my>
2022-04-27 12:10:25 +0700

    Remove unique equalt to true

--- database/models.py
+++ database/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Instance(models.Model):
     instance_id = models.AutoField(primary_key=True)
     instance_name = models.CharField(max_length=100, unique=True)
-    instance_status = models.CharField(max_length=100, unique=True, null=True, default="Unknown")
+    instance_status = models.CharField(max_length=100, null=True, default="Unknown")
     instance_ipv4 = models.CharField(max_length=100, unique=True)
-    instance_AWSSecretKey = models.TextField(unique=True, default="Unknown")
-    instance_AWSAccessKey = models.TextField(unique=True, default="Unknown")
+    instance_AWSSecretKey = models.TextField(default="Unknown")
+    instance_AWSAccessKey = models.TextField(default="Unknown")
