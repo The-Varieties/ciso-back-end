2e932b15008d005e187088a9db04ec1a876ed58c
xlwschn <tidus144@gmail.com>
2022-09-14 19:51:13 +0800

    register services update

--- ciso_back_end/api/register/services.py
+++ ciso_back_end/api/register/services.py
@@ -1,7 +1,7 @@
 from ..users.models import *
 
-def authentice_user(username, password):
-    user = User.objects.get(user_username=username, user_password=password)
+def authentice_user(username, firstname, lastname, email, password):
+    user = User.objects.get(user_username=username, user_firstname=firstname, user_lastname=lastname, user_email=email, user_password=password)
     if user:
         return user.user_id
     return None
