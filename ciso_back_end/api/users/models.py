aa8825329364e206899d3b6e525e6bb9b0cd8d9e
xlwschn <tidus144@gmail.com>
2022-09-16 19:42:36 +0800

    modified user module

--- ciso_back_end/api/users/models.py
+++ ciso_back_end/api/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from ciso_back_end.api.instances.models import Instances
+
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
@@ -8,3 +10,4 @@ class User(models.Model):
     user_lastname = models.CharField(max_length=100)
     user_email = models.CharField(max_length=100)
     user_password = models.CharField(max_length=100, default="Unknown", null=True)
+    instance = models.ForeignKey(Instances, related_name='instances', on_delete=models.CASCADE)
--- ciso_back_end/api/users/serializers.py
+++ ciso_back_end/api/users/serializers.py
@@ -1,8 +1,9 @@
 from ciso_back_end.api.users.models import User
 from rest_framework import serializers
-
+from ciso_back_end.api.instances.serializers import InstanceSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    instances = InstanceSerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = ('user_id',
@@ -10,4 +11,5 @@ class UserSerializer(serializers.ModelSerializer):
                   'user_firstname',
                   'user_lastname',
                   'user_email',
-                  'user_password')
+                  'user_password',
+                  'instances')
